00013ea9e8a0ffdc5c523dfb7e2e07914cbdf75b
Kiahn Cutlip <100371967+KiahnC@users.noreply.github.com>
2024-10-21 21:36:42 -0700

    Update Random Number Guesser.py

--- Random Number Guesser.py
+++ Random Number Guesser.py
@@ -14,13 +14,17 @@ while guessed == False:
     guess = int(input("Guess a number: "))
     if guess < randomnum:
         print("Too Low!")
+        print(" ")
     if guess > randomnum:
         print("Too High!")
+        print(" ")
     if guess == randomnum:
         print("You guessed the correct number! Good job!")
         pa = input("Play again? y/n ")
         if pa == "y":
             continue
+            print(" ")
         if pa == "n":
+            print(" ")
             break
 
