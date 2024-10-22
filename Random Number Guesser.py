7c35af57f05c13a4d18f78794494a263dda2788e
Kiahn Cutlip <100371967+KiahnC@users.noreply.github.com>
2024-10-21 21:31:01 -0700

    Update Random Number Guesser.py

--- Random Number Guesser.py
+++ Random Number Guesser.py
@@ -1,7 +1,25 @@
 import random
 
-lownum = int(input("Enter lowest number: ")
-highnum = int(input("Enter highest number: ")
+lownum = int(input("Enter lowest number: "))
+highnum = int(input("Enter highest number: "))
 
 randomnum = random.randint(lownum, highnum)
+gussed = False
 
+print(" ")
+print("Random number has been generated!")
+print(" ")
+
+while guessed == False:
+    guess = int(input("Guess a number: "))
+    if guess < randomnum:
+        print("Too Low!")
+    if guess > randomnum:
+        print("Too High!")
+    if guess == randomnum:
+        print("You guessed the correct number! Good job!")
+    pa = input("Play again? y/n ")
+    if pa == y:
+        continue
+    if pa == n:
+        break
